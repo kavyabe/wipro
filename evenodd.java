4e8a7c4879cac95b81820ed6c2f1947be20d0744
kavyabe <113851554+kavyabe@users.noreply.github.com>
2024-06-08 13:28:22 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class evenodd
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter n=");
+int n=sc.nextInt();
+if(n%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
